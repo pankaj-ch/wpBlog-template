67c820e91eb9ae0344bb2e138557fdaa01f6fac7
Pankaj Chauhan <pankaj@botsolve.com>
2017-05-30 23:30:31 +0530

    optimized sidebar menu toggle

---
+++ src/js/main/div_add.js
@@ -0,0 +1,3 @@
+// adding blank div to document for dynamic content
+$('body').prepend('<div class="sidebar-menu"></div>');
+$('body').append('<div class="overlay"></div>');
--- src/js/main/sidebar_menu.js
+++ src/js/main/sidebar_menu.js
@@ -1,20 +1,23 @@
 //auto generate sidebar menu
-(function () {
+$(document).ready(function () {
   'use strict';
+
   $('ul.sf-menu').superfish();
+
+  // generates content for sidebar-menu
   var social = '<ul class="social">' + $('ul.social').html() + '</ul>';
   var close = '<a class="back" href="#"><i class="ion-arrow-left-c"></i></a>';
   var menu = $('#nav-main').html();
   $('.sidebar-menu').html(social + menu + close);
+
+  // opens sidebar-menu
   $('.toggle-bar a').click(function(){
     $('body').addClass('menu-open');
-    $('.sidebar-menu').attr('style','transition: left .4s ease-in-out');
-    $('.content-wrapper').attr('style','transition: transform .4s ease-in-out');
-  });
-  $('.sidebar-menu .back').click(function(){
-    $('body').removeClass('menu-open');
-    $('.sidebar-menu').attr('style','transition: left .5s ease-in-out');
-    $('.content-wrapper').attr('style','transition: transform .5s ease-in-out');
   });
 
-})();
+  // Closes sidebar menu
+  $('.sidebar-menu .back, .overlay').click(function(){
+    $('body').removeClass('menu-open');
+  });
+
+});
