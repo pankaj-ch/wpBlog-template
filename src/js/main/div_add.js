b614b8e57b6a7bd8126d634bf128692002bd8a0f
Pankaj Chauhan <pankaj@botsolve.com>
2017-06-02 22:09:36 +0530

    closing search menu button added

--- src/js/main/div_add.js
+++ src/js/main/div_add.js
@@ -2,4 +2,5 @@
 $('body').prepend('<div class="sidebar-menu"></div>');
 $('body').append('<div class="overlay"></div>');
 $('.top-nav').prepend('<div class="toggle-bar"></div>');
+$('.search').append('<i class="ion-close-round"></i>');
       
--- src/js/main/search.js
+++ src/js/main/search.js
@@ -1,4 +1,9 @@
 $(document).ready(function () {
+  'use strict';
+  $('.search .ion-close-round').click(function (e) {
+    $('.search').removeClass('active');
+    e.stopPropagation();
+  });
   $('.search').click(function () {
     $('.search').addClass('active');
   });
